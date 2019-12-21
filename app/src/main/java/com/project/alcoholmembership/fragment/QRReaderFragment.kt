9cf6ba1a1a98340fcb30616812d7b38d9fdccdf4
KimJuhyun92 <juhyun7793@gmail.com>
2019-12-21 23:15:53 +0900

    qr test

--- app/src/main/java/com/project/alcoholmembership/fragment/QRReaderFragment.kt
+++ app/src/main/java/com/project/alcoholmembership/fragment/QRReaderFragment.kt
@@ -1,2 +1,49 @@
 package com.project.alcoholmembership.fragment
 
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.google.zxing.integration.android.IntentIntegrator
+import com.project.alcoholmembership.R
+import kotlinx.android.synthetic.main.fragment_qrreader.view.*
+
+class QRReaderFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view:View = inflater!!.inflate(R.layout.fragment_qrreader, container, false)
+
+        val intentIntegrator = IntentIntegrator(activity)
+        intentIntegrator.setBeepEnabled(false)//바코드 인식시 소리
+
+        view.reader.setOnClickListener{
+            intentIntegrator.initiateScan()
+        }
+
+        return view
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result =  IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if(result != null) {
+            if(result.contents == null) {
+                Log.d("@@test","fail")
+                Toast.makeText(activity, "Cancelled", Toast.LENGTH_SHORT).show()
+            } else {
+                Log.d("@@test","test"+result.contents)
+                Toast.makeText(activity, "Scanned: " + result.contents, Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            Log.d("@@test","fail2")
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+}
--- app/src/main/java/com/project/alcoholmembership/fragment/QRcodeFragment.kt
+++ app/src/main/java/com/project/alcoholmembership/fragment/QRcodeFragment.kt
@@ -1,2 +1,43 @@
 package com.project.alcoholmembership.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.MultiFormatWriter
+import com.journeyapps.barcodescanner.BarcodeEncoder
+import com.project.alcoholmembership.R
+import kotlinx.android.synthetic.main.fragment_qrcode.view.*
+
+class QRcodeFragment : Fragment() {
+
+    private val text = null
+    private val ivQrcode = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view:View = inflater!!.inflate(R.layout.fragment_qrcode, container, false)
+
+        val text = "test"
+
+        val multiFormatWriter : MultiFormatWriter = MultiFormatWriter()
+        try {
+            val bitMatrix = multiFormatWriter.encode(text, BarcodeFormat.QR_CODE, 200, 200)
+            val barcodeEncoder = BarcodeEncoder()
+            val bitmap = barcodeEncoder.createBitmap(bitMatrix)
+            view.qrcode.setImageBitmap(bitmap)
+        } catch (e: Exception) {
+        }
+
+
+
+        return view
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+    }
+
+}
